3f60cb755ace87e0e1c2174ecc09cecd4c9aa86a
Simon Smulders <simsmulders@gmail.com>
2023-11-06 15:50:39 +0100

    main: first commit

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { Route, Router } from 'svelte-routing';
+	import Detail from "./library/Detail.svelte";
+	import Library from "./library/Library.svelte";
+	import Create from './create/Create.svelte';
+	
+	export let url = ''
+</script>
+
+<Router {url}>
+	<main>
+		<Route path="/create">
+			<Create />
+		</Route>
+		<Route path="/books/:id" let:params>
+			<Detail id={params.id}/>
+		</Route>
+		<Route path="/">
+			<Library />
+		</Route>
+	</main>
+</Router>
+
+<style>
+	main {
+		padding: var(--spacingLarge);
+	}
+</style>
